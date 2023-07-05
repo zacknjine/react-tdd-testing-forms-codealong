0816a149dca7e9c3999a7a2316ffbcb2e60c2fd9
Liz Burton <liz.burton147@gmail.com>
2023-07-05 16:57:20 -0400

    fix CodeGrade issue

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom';
+
 import App from "../App";
 
 // Pepperoni checkbox
